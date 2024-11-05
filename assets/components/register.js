d400a251e0926d2b683e2c66728aea4072b46fb0
Yang Fu <yang.fu3785@gmail.com>
2024-11-05 21:09:25 +0100

    form

--- assets/components/register.js
+++ assets/components/register.js
@@ -13,6 +13,41 @@ const Register = ()=>{
         </div>
         <img id="doc" src={profile} alt="photo"/>
     </div>
+
+    <div id="formRe">
+        <form action="/patient/account/new" method="POST">
+
+            <label for="civilite">Civilité :</label>
+            <select id="civilite" name="civilite">
+                <option value="monsieur">Monsieur</option>
+                <option value="madame">Madame</option>
+                <option value="autre">Autre</option>
+            </select>
+
+            <label>Nom</label>
+            <input type="text" name="name" id="name" required/>
+
+            <label>Prénom</label>
+            <input type="text" name="firstname" id="firstname" required/>
+
+            <label>Adresse</label>
+            <input type='text' name="adresse" id="adresse" required/>
+
+            <label>Tel</label>
+            <input type='text' name="tel" id="tel" required/>
+
+            <label>Email</label>
+            <input type="email" name="email" id="email" required />
+
+            <button>Inscrire</button>
+        </form>
+        <a href="#">Retour</a>
+    </div>
+    <footer>
+        <h4>copyright Maurice Olivier</h4>
+          <a href='#'>Condition général</a>
+          <div></div>
+    </footer>
         
       
      
